79ba184e1bab011501e917a361281ae164663fd2
Naba-Alali <S202268320@kfupm.edu.sa>
2026-04-01 11:27:44 +0300

    Todo3

--- calculator.js
+++ calculator.js
@@ -221,3 +221,27 @@ import _ from "lodash";
 
 const operation = process.argv[2];
 const numbers = process.argv.slice(3);
+
+if (!isValidOperation(operation)) {
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide");
+  process.exit(1);
+}
+const nums = parseNumbers(numbers);
+let result;
+
+switch (operation) {
+  case "add":
+    result = add(nums);
+    break;
+  case "subtract":
+    result = subtract(nums);
+    break;
+  case "multiply":
+    result = multiply(nums);
+    break;
+  case "divide":
+    result = divide(nums);
+    break;
+}
+
+console.log(`Result: ${result}`);
